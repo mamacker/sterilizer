bb4e8bec4bd9783e07662f8961a1a24907c5388e
Matthew Amacker <matt@soovle.com>
2020-04-05 05:20:26 +0100

    Broken thread for timer... its a start.

--- app.py
+++ app.py
@@ -48,16 +48,30 @@ t0.start();
 def report_state():
     return json.dumps({'ison': light_state, 'safety_state': safety_state})
 
-def turn_on():
-    global light_state
+on_thread = None
+def timed_turn_off(duration):
+    try:
+        time_per_loop = .2
+        while(duration > 0):
+            time.sleep(time_per_loop)
+            duration = duration - time_per_loop
+        turn_off()
+    except:
+        print("Cancelled timer.")
+
+def turn_on(duration):
+    global light_state, on_thread
     if is_safe():
         light_state = True
         led.on()
+        if (on_thread != None):
+            on_thread.raise_exception()
+        on_thread = threading.Thread(target=timed_turn_off, args=(duration));
+        on_thread.start();
     else:
         light_state = False
         led.off()
     return report_state()
-
 
 class StreamingOutput(object):
     def __init__(self):
@@ -119,7 +133,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             if 'duration' in query_components:
                 duration = query_components["duration"][0]
 
-            self.wfile.write(turn_on().encode())
+            self.wfile.write(turn_on(duration).encode())
         elif self.path == '/off':
             self.send_response(200)
             self.send_header('Content-Type', 'text/javascript')
